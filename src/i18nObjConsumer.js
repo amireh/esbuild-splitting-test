abb13c348ffcce3fe077e61a36f89dfeace95676
Ahmad Amireh <ahmad@amireh.net>
2021-10-13 15:56:57 -0600

    build with webpack

--- src/i18nObjConsumer.js
+++ src/i18nObjConsumer.js
@@ -1,4 +1,4 @@
-import './i18nObj'
+import './i18nObj.js'
 
 const container = typeof global === 'undefined' ? window : global
 container.evaluations = container.evaluations || []
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import './translations'
-import './i18nObjConsumer'
-import('./i18nObjConsumer')
+import './translations.js'
+import './i18nObjConsumer.js'
+import('./i18nObjConsumer.js')
 
 const container = typeof global === 'undefined' ? window : global
 console.log(container.evaluations)
